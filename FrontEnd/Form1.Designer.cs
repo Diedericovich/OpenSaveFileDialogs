b7da7cca941b9ed779b3b53e08673efc4be6b02f
Michiel Wouters <michiel_woute64@hotmail.com>
2021-01-27 09:55:09 +0100

    Add project files.

---
+++ FrontEnd/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace FrontEnd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ofdImageSelector = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(249, 261);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select An Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ofdImageSelector
+            // 
+            this.ofdImageSelector.FileName = "openFileDialog1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(13, 324);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(248, 20);
+            this.txtText.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(249, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save Text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 465);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog ofdImageSelector;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ FrontEnd/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FrontEnd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ofdImageSelector.Title = "Choose an image";
+            ofdImageSelector.Filter = "png files, jpg files(*.png, *.jpg)|*.png;*.jpg|txt files(.txt)|*.txt|All Files|*.*";
+
+            if (ofdImageSelector.ShowDialog() == DialogResult.OK)
+            {
+                string path = ofdImageSelector.FileName;
+
+                pictureBox1.Load(path);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = txtText.Text;
+
+            saveFileDialog1.Filter = "txt files|*.txt";
+            saveFileDialog1.InitialDirectory = "C:\\";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, text);
+            }
+        }
+    }
+}
